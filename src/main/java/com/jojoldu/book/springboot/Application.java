5878a1d65bd431422eb6707a964fea44dd6127f8
Chaerimy0907 <qkrcofus1026@daum.net>
2025-09-08 11:48:19 +0900

    Initial commit without sensitive files

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+// 스프링부트의 자동 설정, 스프링 bean 읽기와 생성을 모두 자동
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
